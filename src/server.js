0761c54e0d7df15b60ac061c758dc838ba2006b6
조현일 <himzei@gmail.com>
2022-10-12 23:11:28 +0900

    middle ware

--- src/server.js
+++ src/server.js
@@ -4,11 +4,22 @@ const PORT = 4000;
 
 const app = express();
 
-const handleHome = (req, res) => {
-  return res.end;
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
 };
 
+const handleHome = (req, res) => {
+  return res.send("im good");
+};
+
+const handleLogin = (req, res) => {
+  return res.send("login");
+};
+
+app.use(logger);
 app.get("/", handleHome);
+app.get("/login", handleLogin);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
