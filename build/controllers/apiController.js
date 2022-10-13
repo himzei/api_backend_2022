ebf894c7819543afcb4ab42a1c462eae53beb3a5
조현일 <himzei@gmail.com>
2022-10-13 12:27:01 +0900

    server build

--- build/controllers/apiController.js
+++ build/controllers/apiController.js
@@ -3,11 +3,10 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.itemNewAll = void 0;
+exports.itemNewSpecial = exports.itemNewAll = exports.itemEditorChoice = exports.blogBest = exports.bestseller = void 0;
 var _axios = _interopRequireDefault(require("axios"));
+var _express = require("express");
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-var TTB_KEY = "ttbhimzei1056002";
-
 // ItemNewAll : 신간 전체 리스트
 // ItemNewSpecial : 주목할 만한 신간 리스트
 // ItemEditorChoice : 편집자 추천 리스트
@@ -15,11 +14,62 @@ var TTB_KEY = "ttbhimzei1056002";
 // Bestseller : 베스트셀러
 // BlogBest : 블로거 베스트셀러 (국내도서만 조회 가능
 
-var QUERY_TYPE = "ItemNewAll";
-var BASE_PATH = "http://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=".concat(TTB_KEY, "&QueryType=").concat(QUERY_TYPE, "&MaxResults=10&start=1&SearchTarget=Book&output=JS&Version=20131101");
+var TTB_KEY = "ttbhimzei1056003";
+var BASE_URL = "http://www.aladin.co.kr/ttb/api/ItemList.aspx";
+// 리스트 수
+var RESUSTS = 10;
 var itemNewAll = function itemNewAll(req, res) {
-  _axios["default"].get(BASE_PATH).then(function (response) {
-    return response.data;
+  var QUERY_TYPE = "ItemNewAll";
+  var END_POINT = "".concat(BASE_URL, "?ttbkey=").concat(TTB_KEY, "&QueryType=").concat(QUERY_TYPE, "&MaxResults=").concat(RESUSTS, "&start=1&SearchTarget=Book&output=JS&Version=20131101");
+  (0, _axios["default"])({
+    method: "get",
+    url: END_POINT
+  }).then(function (response) {
+    return res.send(response.data.item);
   });
 };
 exports.itemNewAll = itemNewAll;
+var itemNewSpecial = function itemNewSpecial(req, res) {
+  var QUERY_TYPE = "ItemNewSpecial";
+  var END_POINT = "".concat(BASE_URL, "?ttbkey=").concat(TTB_KEY, "&QueryType=").concat(QUERY_TYPE, "&MaxResults=").concat(RESUSTS, "&start=1&SearchTarget=Book&output=JS&Version=20131101");
+  (0, _axios["default"])({
+    method: "get",
+    url: END_POINT
+  }).then(function (response) {
+    return res.send(response.data.item);
+  });
+};
+exports.itemNewSpecial = itemNewSpecial;
+var itemEditorChoice = function itemEditorChoice(req, res) {
+  var QUERY_TYPE = "ItemEditorChoice";
+  var END_POINT = "".concat(BASE_URL, "?ttbkey=").concat(TTB_KEY, "&QueryType=").concat(QUERY_TYPE, "&MaxResults=").concat(RESUSTS, "&start=1&SearchTarget=Book&output=JS&Version=20131101");
+  (0, _axios["default"])({
+    method: "get",
+    url: END_POINT
+  }).then(function (response) {
+    return res.send(response.data.item);
+  });
+};
+exports.itemEditorChoice = itemEditorChoice;
+var bestseller = function bestseller(req, res) {
+  var QUERY_TYPE = "Bestseller";
+  var END_POINT = "".concat(BASE_URL, "?ttbkey=").concat(TTB_KEY, "&QueryType=").concat(QUERY_TYPE, "&MaxResults=").concat(RESUSTS, "&start=1&SearchTarget=Book&output=JS&Version=20131101");
+  (0, _axios["default"])({
+    method: "get",
+    url: END_POINT
+  }).then(function (response) {
+    return res.send(response.data.item);
+  });
+};
+exports.bestseller = bestseller;
+var blogBest = function blogBest(req, res) {
+  var QUERY_TYPE = "BlogBest";
+  var END_POINT = "".concat(BASE_URL, "?ttbkey=").concat(TTB_KEY, "&QueryType=").concat(QUERY_TYPE, "&MaxResults=").concat(RESUSTS, "&start=1&SearchTarget=Book&output=JS&Version=20131101");
+  (0, _axios["default"])({
+    method: "get",
+    url: END_POINT
+  }).then(function (response) {
+    return res.send(response.data.item);
+  });
+};
+exports.blogBest = blogBest;
--- build/routers/apiRouter.js
+++ build/routers/apiRouter.js
@@ -9,5 +9,9 @@ var _apiController = require("../controllers/apiController");
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 var apiRouter = _express["default"].Router();
 apiRouter.get("/itemNewAll", _apiController.itemNewAll);
+apiRouter.get("/itemNewSpecial", _apiController.itemNewSpecial);
+apiRouter.get("/itemEditorChoice", _apiController.itemEditorChoice);
+apiRouter.get("/bestseller", _apiController.bestseller);
+apiRouter.get("/blogBest", _apiController.blogBest);
 var _default = apiRouter;
 exports["default"] = _default;
