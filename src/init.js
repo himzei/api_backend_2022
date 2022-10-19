9d6014c89c285ab73a6ccbfacfa653c338d4b3ba
조현일 <himzei@gmail.com>
2022-10-20 07:50:29 +0900

    re

--- src/init.js
+++ src/init.js
@@ -4,7 +4,7 @@ import "./db";
 import "./models/User";
 import app from "./server";
 
-const PORT = 3000;
+const PORT = 4000;
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
