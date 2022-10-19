0cc63f1e5eaf437d378726e6c8fc0268a0d8f02b
조현일 <himzei@gmail.com>
2022-10-19 22:16:17 +0900

    error

--- build/init.js
+++ build/init.js
@@ -6,7 +6,7 @@ require("./db");
 require("./models/User");
 var _server = _interopRequireDefault(require("./server"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-var PORT = 4000;
+var PORT = 3000;
 var handleListening = function handleListening() {
   return console.log("Server listening on port http://localhost:".concat(PORT));
 };
