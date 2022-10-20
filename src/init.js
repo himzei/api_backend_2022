e9f708e22d28dd13aa0ec7cacfb95e8a16a4d3e7
조현일 <himzei@gmail.com>
2022-10-20 10:59:03 +0900

    port

--- src/init.js
+++ src/init.js
@@ -7,6 +7,6 @@ import app from "./server";
 const PORT = 4000;
 
 const handleListening = () =>
-  console.log(`Server listening on port http://localhost:${PORT}`);
+  console.log(`✅ Server listenting on http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);
