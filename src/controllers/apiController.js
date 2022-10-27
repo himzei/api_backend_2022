5d9a67bd5c0ff0628cd606566ba1478bbf3069f0
조현일 <himzei@gmail.com>
2022-10-27 12:50:53 +0900

    updated

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -19,7 +19,7 @@ export const blogList = (req, res) => {
   const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
   const BLOG_ID = "2670211976855533082";
   const END_POINT = `https://www.googleapis.com/blogger/v3/blogs/${BLOG_ID}/posts?key=${GOOGLE_API_KEY}`;
-  // ("https://www.googleapis.com/blogger/v3/blogs/2670211976855533082/posts?key=AIzaSyASRrLYp25_5PItQW0Bxoo8A_IRAxYoSds");
+
   axios({
     method: "GET",
     url: END_POINT,
