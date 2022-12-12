83882ce9381cddd68ae8c01d6d75f8954e74495b
조현일 <himzei@gmail.com>
2022-12-12 15:01:56 +0900

    c

--- src/init.js
+++ src/init.js
@@ -2,7 +2,7 @@ import "regenerator-runtime";
 import "dotenv/config";
 import "./db";
 import "./models/User";
-import "./models/Favs";
+// import "./models/Favs";
 import app from "./server";
 
 const handleListening = () =>
